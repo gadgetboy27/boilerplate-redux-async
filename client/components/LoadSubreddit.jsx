1810f37578199ee4617cd5501289146be45ea0e8
JamesS <jamessanday@gmail.com>
2019-09-11 15:39:35 +1200

    refactor load subreddit component

--- client/components/LoadSubreddit.jsx
+++ client/components/LoadSubreddit.jsx
@@ -2,13 +2,42 @@ import React from 'react'
 import {connect} from 'react-redux'
 import {fetchPosts} from '../actions'
 
-const LoadSubreddit = ({children, dispatch}) => (
-  <div>
-    <button onClick={() => dispatch(fetchPosts('newzealand'))}>
-      Fetch Posts
-    </button>
-    {children}
-  </div>
-)
+class LoadSubreddit extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      input: ''
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange (e) {
+    this.setState({
+      input: e.target.value
+    })
+  }
+
+  render () {
+    return (
+      <div>
+        <input value={this.state.input} onChange={this.handleChange}></input>
+        <button onClick={() => this.props.dispatch(fetchPosts(this.state.input))}>
+        Fetch Posts
+        </button>
+      </div>
+    )
+  }
+}
+
+// const LoadSubreddit = ({children, dispatch}) => (
+//   <div>
+//     <button onClick={() => dispatch(fetchPosts('niue'))}>
+//       Fetch Posts
+//     </button>
+//     {children}
+//   </div>
+// )
 
 export default connect()(LoadSubreddit)
