d1994b67818364324a0e0976357dad0fd137baff
Henry-Peti <iamgadgetboy@gmail.com>
2020-08-04 05:23:26 +1200

    done

--- client/components/LoadSubreddit.jsx
+++ client/components/LoadSubreddit.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import {connect} from 'react-redux'
 import {fetchPosts} from '../actions'
 
-const LoadSubreddit = ({children, dispatch}) => (
+const LoadSubreddit = ({ children, dispatch }) => (
   <div>
     <button onClick={() => dispatch(fetchPosts('newzealand'))}>
       Fetch Posts
